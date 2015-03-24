b787ab46dd7fd2b2a15345b579539baba48c75d6
Ophelle <ophelleshine@gmail.comgit config --global user.email ophelleshine@gmail.comgit config --global user.email ophelleshine@gmail.com>
2015-03-24 11:17:44 +0100

    Affichage Brasseur

--- js/breweries/breweryAffichageController.js
+++ js/breweries/breweryAffichageController.js
@@ -1,18 +1,5 @@
-module.exports=function($scope,config,$location,modalService, $controller) {
-    $controller('BreweryAddController', {$scope: $scope});
+module.exports=function($scope,rest,$timeout,$location,config,$route,save) {
 
-    if (angular.isUndefined(config.activeBrewery)) {
-        $location.path("breweries/");
-    }
-    $scope.activeBrewery = config.activeBrewery;
+    $scope.Brewery = config.activeBrewery;
 
-    $scope._affichage = function (brewery) {
-        $scope.data.posted = {
-            "name": brewery.name,
-            "url": brewery.url,
-            "created_at": brewery.created_at,
-            "updated_at": brewery.updated_at
-
-        };
-    }
 }
